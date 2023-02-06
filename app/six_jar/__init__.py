facc855e9927ac3236adcc066c102cf8f31fbeb2
sky <bqt978@gmail.com>
2023-02-06 09:59:50 +0800

    1.降級至python 版本3.9

--- app/six_jar/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint
-
-six_jar_bp = Blueprint('six_jar', __name__)
-
-
-from . import view
--- app/user/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint
-
-user_bp = Blueprint('user', __name__)
-
-
-from . import view
